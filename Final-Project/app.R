e0133614a2ea139bdd7f3e2f6405a9e17ad26903
michaela-marincic <michaela.g.marincic@gmail.com>
2022-12-16 20:21:21 -0500

    Revert "Make a Mistake, App not Running - Revert"
    This reverts commit 8ecad447a61489fd118df750ac218202a5c18ec1.

--- Final-Project/app.R
+++ Final-Project/app.R
@@ -22,7 +22,8 @@ library(leaflet.extras)
 # Read in data
 
 trails <- st_read("./Allegheny_County_Trails_Locations.geojson")
-facilities <- st_read("Allegheny_County_Park_Features.geojson") 
+facilities <- st_read("Allegheny_County_Park_Features.geojson")
+
 icons <- awesomeIcons(
   icon = "circle",
   markerColor = "#2b680c"
@@ -103,6 +104,8 @@ server <- function(input, output) {
     return(newTrails)
   })
   
+  
+  
   # Subset to data Only on screen
   onScreen <- reactive({
     req(input$map_bounds)
@@ -117,6 +120,7 @@ server <- function(input, output) {
     return(onscreen)
     })
   
+  
   # Facility Filtered Data
   facData <- reactive(({
     facilities <- facilities %>%
@@ -124,7 +128,6 @@ server <- function(input, output) {
       subset(select = c("FeatureName", "Facility_Type", "Center", "Website", "Capacity", "ADA_Accessible_Facility"))
     return(facilities)
   }))
-
   
   # Add a basemap
     output$map <- renderLeaflet({
@@ -204,7 +207,7 @@ server <- function(input, output) {
     })
 
     # Produce data table for download
-    output$parks <- DT::renderDataTable(facsOnScreen(), 
+    output$parks <- DT::renderDataTable(facData(), 
                                         rownames = FALSE, 
                                         options=list(columnDefs = list(list(visible=FALSE, targets=c(6)))) # Hide geometry column
                                         )
